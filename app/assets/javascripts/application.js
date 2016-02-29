be61b2800ce0d9b057afaeab5686a6ee07a522b2
saqibroy <saqibsohail007@gmail.com>
2016-02-29 15:50:33 +0500

    deploy

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,20 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require js/jquery
+//= require js/bootstrap.min
+//= require js/classie
+//= require js/cbpAnimatedHeader
+//= require js/jqBootstrapValidation
+//= require js/contact_me
+//= require js/freelancer
+
