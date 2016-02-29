f73b25116977dfdc7b65b424fd2952a1cf6e0bca
saqibroy <saqibsohail007@gmail.com>
2016-02-29 16:58:34 +0500

    c

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  Bundler.require(*Rails.groups(assets: %w(development test production)))
+ 
   config.serve_static_files = true
   config.cache_classes = true
 
